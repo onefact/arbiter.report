e9a3de125637abf62a97a2f2d59f309a4dd77e7a
Aditi Kamath <adikamath3@gmail.com>
2022-11-19 22:56:33 +0530

    network graph hover condition added

--- src/components/dashboard/network/FollowerGroup.js
+++ src/components/dashboard/network/FollowerGroup.js
@@ -43,36 +43,12 @@ const FollowerGroup = ({data}) => {
             >
               <ForceGraph2D
                 height={450}
+                nodeLabel={(node) => node.id}
                 nodeColor = {node => node.group === 1 ? 'blue' : (node.group === 2 ? 'gray' : (node.group === 3 ?'green' : 'orange'))}
-               
-                // nodeAutoColorBy="group"
-         
-       
-                // linkColor={d=>theme.palette.divider}
+          
                 graphData={data}
                 
-                // graphData={{ nodes: [
-                //   {id:1, name: "name1", val:1},
-                //   {id:2, name: "name2", val:2},
-                //   {id:3, name: "name3", val:1},
-                //   {id:4, name: "name4", val:5},
-                //   {id:5, name: "name5", val:3},
-                //   {id:6, name: "name6", val:1},
-                //   {id:7, name: "name7", val:2},
-                //   {id:8, name: "name8", val:1},
-                //   {id:9, name: "name9", val:4},
-                // ], links: [
-                  
-                //   {source: 1, target: 2, },
-                //   {source: 2, target: 3},
-                //   {source: 1, target: 3},
-                //   {source: 1, target: 4},
-                //   {source: 1, target: 8},
-
-                //   {source: 6, target: 7},
-                //   {source: 6, target: 5},
-                //   {source: 6, target: 9},
-                // ] }}
+           
               />
           </Box>
         </Box>
